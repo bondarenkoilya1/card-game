87c8bd6d45d4f563210073df060c122be2e26625
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-06-01 13:36:10 +0300

    create first version of select component with needed functionality, write ideas how to realize card set selection

--- src/layout/SectionManageCardSet/SectionManageCardSet.tsx
+++ src/layout/SectionManageCardSet/SectionManageCardSet.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   ManageCardSetListStyled,
@@ -11,7 +11,6 @@ import {
 import { Error } from "src/components";
 import { CardSet } from "src/components/CardSet";
 
-// import { CardSetProps } from "src/types";
 import { useCardSetsStore } from "src/store";
 
 import { useCardSetHTTPMethod } from "src/hooks";
@@ -19,18 +18,25 @@ import { useCardSetHTTPMethod } from "src/hooks";
 export const SectionManageCardSet = () => {
   const { cardSets, isLoading, error } = useCardSetsStore();
   const { fetchCardSets, deleteCardSet, updateCardSet } = useCardSetHTTPMethod();
-  // const [cardSetNames, setCardSetNames] = useState<string[]>([]);
+  const [cardSetNames, setCardSetNames] = useState<string[]>([]);
+  const [currentCardSet, setCurrentCardSet] = useState<string>(cardSetNames[0]);
 
   useEffect(() => {
     fetchCardSets();
   }, []);
 
-  // useEffect(() => setCardSetNames(cardSets.map((cardSet) => cardSet.cardSetName)), [cardSets]);
-  // console.log(cardSetNames);
+  useEffect(() => setCardSetNames(cardSets.map((cardSet) => cardSet.cardSetName)), [cardSets]);
 
-  // Here render list by current selected card set name
+  // console.log(cardSets.filter((cardSet) => cardSet.cardSetName === "xcv"));
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentCardSet(event.target.value || cardSetNames[0]);
+  };
 
   const renderCardSetList = () => {
+    /* Here we will render _currentSet;
+    We will set _currentSet when select has changed
+    And filter cardSets to find set with the identical name with selected option */
     const firstSet = cardSets?.[0];
     if (!firstSet) return <p>No card sets available.</p>;
 
@@ -59,6 +65,22 @@ export const SectionManageCardSet = () => {
   return (
     <SectionManageCardSetStyled>
       {error && renderSupportSection()}
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="select_card-sets">Select card set</label>
+        <select
+          name="card-sets"
+          id="select_card-sets"
+          style={{ width: "fit-content" }}
+          onChange={handleSelect}>
+          {cardSetNames.map((cardSetName) => (
+            <option key={cardSetName} value={cardSetName}>
+              {cardSetName}
+            </option>
+          ))}
+        </select>
+      </div>
+
       {error && <Error unspecifiedErrorMessage={`${error}. Try again or contact support`} />}
       {isLoading ? "Loading..." : renderCardSetList()}
     </SectionManageCardSetStyled>
