94d37d77cbaea0377817c13616f8d48e7c1bfae1
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-07-16 09:25:24 +0300

    return index file for providers folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 
-import { Providers } from "src/providers/Providers";
+import { Providers } from "src/providers";
 
 createRoot(document.getElementById("root")!).render(<Providers />);
