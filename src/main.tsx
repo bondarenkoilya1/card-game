b5db7b8876114059127cd973e3f4cea06f42a9c8
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-07-16 09:20:03 +0300

    remove index file from providers folder to resolve conflict

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 
-import { Providers } from "src/providers";
+import { Providers } from "src/providers/Providers";
 
 createRoot(document.getElementById("root")!).render(<Providers />);
