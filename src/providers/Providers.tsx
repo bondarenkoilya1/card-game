d5879977799418d2a25cf13d6dcc3811380cf294
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-07-17 16:50:47 +0300

    restore providers folder to resolve conflict

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,34 @@
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer, ToastContainerProps } from "react-toastify";
+
+import { ThemeProvider } from "@emotion/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { router } from "src/router";
+
+import { defaultTheme } from "src/themes";
+
+const queryClient = new QueryClient();
+
+export const toastContainerOptions: ToastContainerProps = {
+  position: "bottom-right",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: false,
+  newestOnTop: true
+};
+
+export const Providers = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <ToastContainer {...toastContainerOptions} />
+      <ThemeProvider theme={defaultTheme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
