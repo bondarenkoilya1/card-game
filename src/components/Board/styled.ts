313f9504915e6afbb4e564e3d791bfff5f472a0b
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-07-21 11:34:28 +0300

    add brown background for game page for better loading experience

--- src/components/Board/styled.ts
+++ src/components/Board/styled.ts
@@ -2,7 +2,7 @@ import { css } from "@emotion/react";
 
 import styled from "@emotion/styled";
 
-export const BoardStyled = styled.div`
+export const BoardStyled = styled.section`
   width: 100%;
   margin-top: 5%;
   display: flex;
--- src/pages/App/Game/styled.ts
+++ src/pages/App/Game/styled.ts
@@ -5,9 +5,12 @@ import styled from "@emotion/styled";
 import woodImage from "../../../assets/photos/wood.jpg";
 
 export const GamePageStyled = styled.div`
-  background:
-    linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${woodImage}) center/cover no-repeat;
+  background-color: #3a0707;
+  background-image:
+    linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${woodImage});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   height: 100%;
   width: 100%;
 `;
